f41b2976028afe23ff497439372d5ebf78b41c30
Oleg Lytvynenko (Personal) <oleg.v.lytvynenko@gmail.com>
2021-04-10 05:45:11 +0300

    stable

--- src/main.cpp
+++ src/main.cpp
@@ -80,19 +80,11 @@
 #define ST_CMD_FRCTRL2      0xC6
 #define ST_CMD_PWCTR1		0xD0
 
-#define MADCTL_MY  0x80
-#define MADCTL_MX  0x40
-#define MADCTL_MV  0x20
-#define MADCTL_ML  0x10
-#define MADCTL_MH  0x04
-
 #define TFT_CASET		0x2A
 #define TFT_RASET		0x2B
 #define TFT_RAMWR		0x2C
 #define TFT_RAMRD		0x2E
 #define TFT_CMD_PIXFMT	0x3A
-
-#define TFT_CMD_DELAY	0x80
 
 // Flags for TFT_MADCTL
 #define TFT_MAD_MY  0x80
@@ -109,29 +101,31 @@
 #define DISP_COLOR_BITS_16	0x55
 #define TFT_RGB_BGR 0x00
 
+#define TFT_CMD_DELAY	0x80
+
 static const unsigned char ST7789V_init[] = {
-18,
+4,
 //   22,                   					        // X commands in list
 //    TFT_CMD_SWRESET, TFT_CMD_DELAY, 200,      	//  1: Software reset, no args, w/200 ms delay   
-    TFT_CASET, 4, 0, 0x1C, 0, 0xD3,
-    TFT_RASET, 4, 0, 0, 0, 0x5F,
-    TFT_MADCTL, 1, 0,
-    TFT_CMD_PIXFMT, 1, DISP_COLOR_BITS_16,            // *** INTERFACE PIXEL FORMAT: 0x66 -> 18 bit; 0x55 -> 16 bit
-    //TFT_CMD_RAMCTRL, 2, 0, 8,
-    TFT_CMD_FRMCTR2, 5, 0xC, 0xC, 0, 0x33, 0x33,
-    TFT_ENTRYM, 1, 0x72,
-    ST_CMD_VCOMS, 1, 0x3B,
-    TFT_CMD_PWCTR1, 1, 0x2C,
-//    TFT_CMD_PWCTR3, 1, 0x1,
-    TFT_CMD_PWCTR4, 1, 0x14,
-    TFT_CMD_PWCTR5, 1, 0x20,
-    ST_CMD_FRCTRL2, 1, 0xF,
-    ST_CMD_PWCTR1, 2, 0xA4, 0xA1,
-    TFT_CMD_GMCTRP1, 14, 0xD0,0x10,0x16,0xA,0xA,0x26,0x3C,0x53,0x53,0x18,0x15,0x12,0x36,0x3C,
-    TFT_CMD_GMCTRN1, 14, 0xD0,0x11,0x19,0xA,0x9,0x25,0x3D,0x35,0x54,0x17,0x15,0x12,0x36,0x3C,
+//     TFT_CASET, 4, 0, 0x1C, 0, 0xD3,
+//     TFT_RASET, 4, 0, 0, 0, 0x5F,
+//     TFT_MADCTL, 1, 0,
+//     TFT_CMD_PIXFMT, 1, DISP_COLOR_BITS_16,            // *** INTERFACE PIXEL FORMAT: 0x66 -> 18 bit; 0x55 -> 16 bit
+//     //TFT_CMD_RAMCTRL, 2, 0, 8,
+//     TFT_CMD_FRMCTR2, 5, 0xC, 0xC, 0, 0x33, 0x33,
+//     TFT_ENTRYM, 1, 0x72,
+//     ST_CMD_VCOMS, 1, 0x3B,
+//     TFT_CMD_PWCTR1, 1, 0x2C,
+// //    TFT_CMD_PWCTR3, 1, 0x1,
+//     TFT_CMD_PWCTR4, 1, 0x14,
+//     TFT_CMD_PWCTR5, 1, 0x20,
+//     ST_CMD_FRCTRL2, 1, 0xF,
+//     ST_CMD_PWCTR1, 2, 0xA4, 0xA1,
+//     TFT_CMD_GMCTRP1, 14, 0xD0,0x10,0x16,0xA,0xA,0x26,0x3C,0x53,0x53,0x18,0x15,0x12,0x36,0x3C,
+//     TFT_CMD_GMCTRN1, 14, 0xD0,0x11,0x19,0xA,0x9,0x25,0x3D,0x35,0x54,0x17,0x15,0x12,0x36,0x3C,
 //   0xE9, 3, 0x5, 0x5, 0x1,
     TFT_CMD_SLPOUT, TFT_CMD_DELAY, 120,				//  Sleep out,	//  120 ms delay
-    TFT_INVONN, 1, 0,
+    TFT_INVONN, TFT_CMD_DELAY, 10,
     TFT_CMD_NORON, TFT_CMD_DELAY, 10,
     TFT_DISPON, TFT_CMD_DELAY, 250
 };
@@ -140,6 +134,7 @@ struct GPIO
 {
     int vd;
     int pin;
+    int is_x;
 };
 
 #define BUF_SZ 184*96
@@ -155,20 +150,6 @@ void gpioClose(GPIO* gpio)
 {
     if (gpio->vd)
         close(gpio->vd);
-}
-
-void gpioSet(GPIO* gpio, int state)
-{
-    if ( gpio->pin >= 0 && gpio->vd )
-    {
-        write(gpio->vd, (state == 1) ? "1":"0", 1u);
-        // printf("Set pin %d state to %d\n", gpio->pin, state);
-    }
-    else 
-    {
-        perror("Error setting gpio value");
-        exit(5);
-    }
 }
 
 void gpioDirection(GPIO* gpio, int mode)
@@ -192,7 +173,26 @@ void gpioDirection(GPIO* gpio, int mode)
     }
 }
 
-void gpioInit(GPIO* gpio, int pin, int mode, int state)
+void gpioSet(GPIO* gpio, int state)
+{
+    if ( gpio->pin >= 0 && gpio->vd )
+    {
+        if (gpio->is_x) {
+            gpioDirection(gpio, (state == 0));
+        }
+        else {
+            write(gpio->vd, (state == 1) ? "1":"0", 1u);
+        }
+        // printf("Set pin %d state to %d\n", gpio->pin, state);
+    }
+    else 
+    {
+        perror("Error setting gpio value");
+        exit(5);
+    }
+}
+
+void gpioInit(GPIO* gpio, int pin, int mode, int state, int is_x)
 {
     char tmp[64];
     gpio->pin = -1;
@@ -210,10 +210,9 @@ void gpioInit(GPIO* gpio, int pin, int mode, int state)
         if ( gpio->vd )
         {
             gpio->pin = pin;
+            gpio->is_x = is_x;
             // printf("Set pin %d mode to %d\n", gpio->pin, mode);
-            if (mode == 1) {
-                gpioSet(gpio, state);
-            }
+            gpioSet(gpio, state);
         }
         else
         {
@@ -262,7 +261,7 @@ int lcd_data(int spi, int is_cmd, char* txbuf, int len)
         printf("\n");
     }
     else {
-        printf("LCD Data. Size:%d\n", is_cmd, len);
+        printf("LCD Data. Size:%d\n", len);
     }
     return ioctl(spi, SPI_IOC_MESSAGE(1), xfer);
 }
@@ -317,7 +316,24 @@ void lcd_backlight(int level)
     }
 }
 
-int main(int argc, char **argv)
+void lcd_hw_reset()
+{
+    gpioInit(&rstPin, 55, 0, 0, 1);
+    gpioInit(&xPin, 96, 0, 0, 1);
+
+    delay(50);
+    gpioSet(&rstPin, 0);
+    gpioSet(&xPin, 0);
+    delay(50);
+    gpioSet(&rstPin, 1);
+    gpioSet(&xPin, 1);
+    delay(120);
+
+    gpioClose(&rstPin);
+    gpioClose(&xPin);
+}
+
+int lcd_init()
 {
     lcd_backlight(10);
 
@@ -332,20 +348,17 @@ int main(int argc, char **argv)
     ioctl(spi, SPI_IOC_RD_MAX_SPEED_HZ, (char *)&speed);
     printf("LCD SPI Mode=%04x Bits=%02d Speed=%04d\n",mode,bits,speed);
 
-    gpioInit(&dcPin, 110, 1, 1);
-    // gpioInit(&rstPin, 55, 0, 0);
-    // gpioInit(&xPin, 96, 0, 0);
+    gpioInit(&dcPin, 110, 1, 1, 0);
 
-    // delay(50);
-    // gpioDirection(&rstPin, 1);
-    // gpioDirection(&xPin, 1);
-    // delay(50);
-    // gpioDirection(&rstPin, 0);
-    // gpioDirection(&xPin, 0);
-    // delay(120);
-
+    //lcd_hw_reset();
+    
     int status = commandList(spi, ST7789V_init);
-    printf("LCD Initialized\n");
+    return spi;
+}
+
+int main(int argc, char **argv)
+{
+    int spi = lcd_init();
 
     uint16_t colors1[3] = {0xF800, 0x7E0, 0x1F};
     uint16_t colors2[3] = {0x1F, 0xF800, 0x7E0};
