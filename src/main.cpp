796fe787622462890426c11d4b969fedda84c849
Oleg Lytvynenko (Personal) <oleg.v.lytvynenko@gmail.com>
2021-05-04 15:30:17 +0300

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,10 @@
 #include "utils.h"
-#include "lcd.h"
-#include "imu.h"
-#include "cam.h"
-#include "pcm.h"
 
 extern void lcd_demo();
 extern void imu_demo();
 extern void cam_demo();
 extern void pcm_demo();
 extern void spine_demo();
-
-
 
 int main(int argc, char **argv) {
     spine_demo();
